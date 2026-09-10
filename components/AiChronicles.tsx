7e31d71e4681dbbcc9787672fa9f2727564f343c
Ankit Samriwal <ankitsamriwal@gmail.com>
2026-09-11 00:25:25 +0400

    Chronicle 59: Seven Posts, One Password Reset (Sep 10 ship log)

--- components/AiChronicles.tsx
+++ components/AiChronicles.tsx
@@ -12,8 +12,9 @@ import { articles55 } from '../data/chronicles-55';
 import { articles56 } from '../data/chronicles-56';
 import { articles57 } from '../data/chronicles-57';
 import { articles58 } from '../data/chronicles-58';
+import { articles59 } from '../data/chronicles-59';
 
-const articles = [...baseArticles, ...articles21To25, ...articles26To30, ...articles31To35, ...articles36To40, ...articles41To45, ...articles46To50, ...articles51To53, ...articles54, ...articles55, ...articles56, ...articles57, ...articles58];
+const articles = [...baseArticles, ...articles21To25, ...articles26To30, ...articles31To35, ...articles36To40, ...articles41To45, ...articles46To50, ...articles51To53, ...articles54, ...articles55, ...articles56, ...articles57, ...articles58, ...articles59];
 
 const heroImages = [
   'https://images.unsplash.com/photo-1677442136019-21780ecad995?auto=format&fit=crop&q=85&w=1400',
